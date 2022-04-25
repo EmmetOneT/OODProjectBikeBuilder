65efb5a43b7f1b64d7eedbdd0171b4795a390d0d
EmmetOneT <emmetcarroll94@gmail.com>
2022-04-25 19:27:23 +0100

    Add project files.

---
+++ S00213942ProjectOOD/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace S00213942ProjectOOD
+{
+    public partial class MainWindow : Window
+    {
+        ABikeContainer db = new ABikeContainer();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //reacts when selecting bike brand
+        //private void cbxBrand_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    Frame selectedFrame = cbxBrand.SelectedItem as Frame;
+
+        //    if (selectedFrame != null)
+        //    {
+        //        var FrameResults = from i in db.Frames
+        //                           select i.Description;
+
+
+
+        //    }
+        //}
+
+        private void cbxDrivetrain_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lbxBasket_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        //Fills Combo boxes when window is loaded
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+     
+        }
+
+
+        private void lbxFrame_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+                
+            
+        }
+
+        private void lbxFork_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void lbxDrive_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
